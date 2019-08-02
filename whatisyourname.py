fa6b830b67fa113f3d6030a4faed0304e168c20d
wolfcomm <wolfcom@gmail.com>
2019-08-02 12:46:02 +0400

    Update whatisyourname.py

--- whatisyourname.py
+++ whatisyourname.py
@@ -1,3 +1,8 @@
-name = input('What is your name?\n')
-print('Your name is')
-print(name)
+x = int(input('Enter digit?\n'))
+print('Your digit is:')
+print(x)
+if x > 10 :
+    print('X is greater than 10')
+if x < 10 :
+    print('X is fewer than 10')
+print('All done')
