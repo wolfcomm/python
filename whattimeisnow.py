065c0d6eea2b95828edbd9fbb6e5296fa2168b9c
wolfcomm <wolfcom@gmail.com>
2020-08-26 00:47:21 +0400

    Update whattimeisnow.py

--- whattimeisnow.py
+++ whattimeisnow.py
@@ -1,8 +1,7 @@
 name = input('Как вас зовут? \n')
 print('Привет, ', name, '!', sep='')
 time = int(input('Который сейчас час, ' + name + '?\n'))
-a = time
-print('Понятно, а я думал, что ', a + 1, end='')
+print('Понятно, а я думал, что ', time + 1, end='')
 time = a + 1
 if time < 0 or time >= 25:
     print(' часов не бывает', name, '!', sep='')
