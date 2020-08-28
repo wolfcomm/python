941628ae637e1bd259429a2eb95a919e29dc74de
wolfcomm <wolfcom@gmail.com>
2020-08-28 23:45:42 +0400

    Update whattimeisnow.py

--- whattimeisnow.py
+++ whattimeisnow.py
@@ -5,9 +5,9 @@ print('Понятно, а я думал, что ', time + 1, end='')
 time = time + 1
 if time < 0 or time >= 25:
     print(' часов не бывает', name, '!', sep='')
-elif time >= 22:
+elif time >= 22 or time == 2 or time == 3 or time == 4: #время в диапазоне 0-24
     print(' часа!')
-elif time % 10 == 1:
+elif time == 1 or time == 21: #время в диапазоне 0, 1, 5-21
     print(' час!')
 else:
     print(' часов!')
